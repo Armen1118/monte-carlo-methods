4c5b5f3dd31f5e671059c435b35edc67d70291a1
Armen1118 <armenpoghosyan0806@gmail.com>
2026-01-24 23:55:31 -0800

    new things

--- hw2.py
+++ hw2.py
@@ -6,3 +6,9 @@ import random
 #ls brings up all the files and their names
 
 #pwd tells you where you are right now
+
+#GIT THINGS
+#git status
+#git add file name
+# git commit -m "i changed blah blah"
+#git push
